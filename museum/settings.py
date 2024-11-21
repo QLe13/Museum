39489477ce809a9316875422f1b4886e0fbb3315
Q <q@qs-macbook-air.trinity.local>
2024-11-20 21:36:35 -0600

    fixed popularity report trigger

--- museum/settings.py
+++ museum/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'museum_app',
-    'rest_framework',
-    'pgtrigger'
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- museum_app/apps.py
+++ museum_app/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class MuseumAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'museum_app'
+
+    def ready(self):
+        import museum_app.signals
