6d71f49d824ffd0fb22ed9e6cbd249bd06836864
ddokupil12 <redder.surgery0e@icloud.com>
2024-12-07 19:38:05 -0600

    updated Update functionality to be more abstract

--- museum/settings.py
+++ museum/settings.py
@@ -79,9 +79,9 @@ WSGI_APPLICATION = 'museum.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'museum_db',      # Replace with your DB name
+        'NAME': 'museum',      # Replace with your DB name
         'USER': 'root',           # Replace with your DB username
-        'PASSWORD': '@Chi3v3r$',       # Replace with your DB password
+        'PASSWORD': 'root',       # Replace with your DB password
         'HOST': 'localhost',
         'PORT': '3306',
     }
